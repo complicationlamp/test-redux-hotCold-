6742369e2a2e0e0dae5dad23cf5d4809fcd389ba
Vindaloo <Kyle@Eshas-MBP.domain>
2018-07-26 16:35:30 -0700

    working test in aural status and made feedback test file

--- src/components/aural-status.test.js
+++ src/components/aural-status.test.js
@@ -2,3 +2,14 @@ import React from 'react';
 import {shallow} from 'enzyme';
 
 import {AuralStatus} from './aural-status';
+import { prependOnceListener } from 'cluster';
+
+describe('auralStatus', () => {
+	it('renders without crashing', () => {
+		shallow(<p id="status-readout"
+		className="visuallyhidden"
+		aria-live="assertive"
+		aria-atomic="true"/>
+		);
+	})
+})
