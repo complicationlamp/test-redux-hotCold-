075f8a0ee31716515c9c7e32ec40f45aa577fd8b
Vindaloo <Kyle@Eshas-MacBook-Pro.local>
2018-07-26 08:55:21 -0700

    added make guess test

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,4 +1,4 @@
-import { restartGame, RESTART_GAME } from "./actions";
+import { restartGame, RESTART_GAME, makeGuess, MAKE_GUESS } from "./actions";
 
 // describe()
 
@@ -9,7 +9,16 @@ describe('restartGame', () => {
 		expect(action.type).toEqual(RESTART_GAME);
 		expect(action.correctAnswer).toEqual(correctAnswer);
 	});
-}):
+});
+
+describe('makeGuess', () => {
+	it('Should compare a guess', () => {
+		const guess = 5;
+		const action = makeGuess(guess);
+		expect(action.type).toEqual(MAKE_GUESS);
+		expect(action.guess).toEqual(guess);
+	});
+});
 
 
 // export const ADD_LIST = 'ADD_LIST';
