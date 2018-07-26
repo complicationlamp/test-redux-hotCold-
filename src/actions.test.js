301da60d9a8951e05f4f3d3435a716c2c03efb76
Vindaloo <Kyle@Eshas-MBP.domain>
2018-07-26 16:09:03 -0700

    added reducer and test WORKING

--- src/actions.test.js
+++ src/actions.test.js
@@ -25,17 +25,3 @@ describe('makeGuess', () => {
 		expect(action.guess).toEqual(guess);
 	});
 });
-
-
-// export const ADD_LIST = 'ADD_LIST';
-// export const addList = title => ({
-//     type: ADD_LIST,
-//     title
-// });
-
-// export const ADD_CARD = 'ADD_CARD';
-// export const addCard = (text, listIndex) => ({
-//     type: ADD_CARD,
-//     text,
-//     listIndex
-// });
---
+++ src/reducer.test.js
@@ -0,0 +1,42 @@
+import reducer from './reducer';
+import {generateAuralUpdate, restartGame, makeGuess} from './actions'
+import { stat } from 'fs';
+
+describe('hotColdReducer', () => {
+	it('should set the intial state when nothing is passed', () => {
+		const state = reducer(undefined, {type: '__UNKNOWN'});
+		expect(state.guesses).toEqual([]);
+		expect(state.feedback).toEqual('Make your guess!');
+		expect(state.auralStatus).toEqual('');
+		expect(state.correctAnswer).toBeGreaterThanOrEqual(0);
+		expect(state.correctAnswer).toBeLessThanOrEqual(100);
+		});
+	});
+
+	describe('restartGame', () => {
+		it('Should clear out exiting data', () => {
+			let state = {
+				guesses: [3, 5],
+			};
+			state = reducer(state, restartGame());
+			expect(state).toEqual({
+				guesses: [],
+				feedback: "Make your guess!",
+				auralStatus: "",
+				correctAnswer: undefined
+			});
+		});
+	});
+
+	describe('makeGuess', () => {
+		it('Should accept guess', () => {
+			let state = {
+				guesses:[]
+			};
+			state = reducer(state, makeGuess(44));
+			expect(state).toEqual({
+				guesses: [44],
+				feedback: "You got it!"
+			})
+		});
+	});
