287dd6f0ae4340f227eed51d608a070117b3cc88
Vindaloo <Kyle@Eshas-MacBook-Pro.local>
2018-07-27 12:31:10 -0700

    dispatch test in guess-form

--- src/components/guess-form.test.js
+++ src/components/guess-form.test.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 
-import guessForm, { GuessForm } from './guess-form';
+import { GuessForm } from './guess-form';
+import { makeGuess} from '../actions';
+import { wrap } from 'module';
 
 describe('<GuessForm/>', () => {
 	it('Should render without crashing', () => {
 		shallow(<guessForm/>)
+	});
+
+	it('Should dispatch makeGuess', () => {
+		const dispatch = jest.fn();
+		const wrapper = mount(<GuessForm dispatch={dispatch}/>);
+		const guess = '33';
+		wrapper.find('input[type="number"]').instance().value = guess;
+		wrapper.simulate('submit');
+		expect(dispatch).toHaveBeenCalledWith(makeGuess(guess));
+	});
+
+	it('Shouold clear the input field after submit', () => {
+		
 	});
 });
